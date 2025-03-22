e92b45fe15fd06df2bad11913887ca546d4e068f
Sushrita Banerjee <mahesh.gaikwad@indexnine.com>
2025-03-22 20:35:29 +0530

    Fixed icon and style chagnes

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,43 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {
+  faComments,
+  faUsers,
+  faRobot,
+  faPaperPlane,
+  faPaperclip,
+  faVideo,
+  faStop,
+  faExpand,
+  faCompress,
+  faTimes,
+  faEllipsisV,
+} from '@fortawesome/free-solid-svg-icons'
 
 import App from './App.vue'
+
+// Add icons to the library
+library.add(
+  faComments,
+  faUsers,
+  faRobot,
+  faPaperPlane,
+  faPaperclip,
+  faVideo,
+  faStop,
+  faExpand,
+  faCompress,
+  faTimes,
+  faEllipsisV,
+)
 
 const app = createApp(App)
 
 app.use(createPinia())
+// Register Font Awesome component globally
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.mount('#app')
